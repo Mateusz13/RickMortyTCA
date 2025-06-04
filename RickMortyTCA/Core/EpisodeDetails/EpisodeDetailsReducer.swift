e686f09acd5ab1f24639d70a4f215f7735aeed13
Mateusz Szafarczyk <szafarczyk.mat@gmail.com>
2025-06-04 15:27:51 +0200

    add EpisodeDetailsView and EpisodeDetailsReducer and connect EpisodeDetailsView to  CharacterDetailsView

---
+++ RickMortyTCA/Core/EpisodeDetails/EpisodeDetailsReducer.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeDetailsReducer.swift
+//  RickMortyTCA
+//
+//  Created by Mateusz Szafarczyk on 04/06/2025.
+//
+
+import Foundation
---
+++ RickMortyTCA/Core/EpisodeDetails/EpisodeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailsView.swift
+//  RickMortyTCA
+//
+//  Created by Mateusz Szafarczyk on 04/06/2025.
+//
+
+import SwiftUI
+
+struct EpisodeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeDetailsView()
+}
